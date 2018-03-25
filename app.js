dca32be64fd9b7a5a415682cb1121dbea28e82f6
squall1744 <squall1744@163.com>
2018-03-25 15:23:23 +0800

    add save function

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 let canvas = document.getElementById('canvas')
 let tools = document.getElementById('tools')
+let downlaod = document.getElementById('download')
+let deleteContent = document.getElementById('delete')
 let eraser = document.getElementById('eraser')
-let brush = document.getElementById('brush')
+let brush = document.getElementById('pen')
+let black = document.getElementById('black')
+let red = document.getElementById('red')
+let blue = document.getElementById('blue')
+let green = document.getElementById('green')
 let ctx = canvas.getContext('2d')
 let use = false
 let lastPoint = {
@@ -11,7 +17,7 @@ let lastPoint = {
 let eraserEnable = false
 
 
-
+// document.body.ontouchstart = e => e.preventDefault()
 autoSetCanvasSize()
 listenToUser(canvas)
 changeTools()
@@ -19,9 +25,9 @@ changeTools()
 
 
 /**********/
-function drawCircle(x,y) {
+function drawCircle(x,y,radius) {
   ctx.beginPath()
-  ctx.arc(x,y,2,0,2*Math.PI)
+  ctx.arc(x,y,radius,0,2*Math.PI)
   ctx.fill()
 }
 
@@ -39,7 +45,7 @@ function drawLine(x,y,x1,y1) {
 
 /*********/
 function eraseDrawing(x,y) {
-  ctx.clearRect(x-5,y-5,10,10)
+  ctx.clearRect(x,y,10,10)
 }
 
 
@@ -72,7 +78,7 @@ function listenToUser(canvas) {
       if(eraserEnable) {
         eraseDrawing(x,y)
       }else {
-        drawCircle(x,y)
+        drawCircle(x,y,2)
       }
     }
 
@@ -88,7 +94,7 @@ function listenToUser(canvas) {
             y:y
           }
           drawLine(lastPoint.x,lastPoint.y,x,y)
-          drawCircle(x,y)
+          drawCircle(x,y,2)
           lastPoint = newPoint
         }
       }
@@ -136,13 +142,66 @@ function listenToUser(canvas) {
 
 /*****************/
 function changeTools() {
+  downlaod.onclick = e => {
+    let url = canvas.toDataURL('image/png')
+    let a = document.createElement('a')
+    document.body.appendChild(a)
+    a.href = url
+    a.download = '我的画'
+    a.target = '_blank'
+    a.click()
+  }
+
+  deleteContent.onclick = e => {
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+  }
+
   eraser.onclick = e => {
     eraserEnable = true
-    tools.className = 'inactive'
+    eraser.classList.add('active')
+    pen.classList.remove('active')
   }
   
-  brush.onclick = e => {
-    eraserEnable = false
-    tools.className = 'active' 
+  pen.onclick = e => {
+    eraserEnable = false   
+    pen.classList.add('active')
+    eraser.classList.remove('active')
   }
+
+  black.onclick = e => {
+    ctx.fillStyle = 'black'
+    ctx.strokeStyle = 'black'
+    black.classList.add('active')
+    red.classList.remove('active')
+    blue.classList.remove('active')
+    green.classList.remove('active')
+  }
+
+  red.onclick = e => {
+    ctx.fillStyle = 'red'
+    ctx.strokeStyle = 'red'
+    red.classList.add('active')
+    black.classList.remove('active')
+    blue.classList.remove('active')
+    green.classList.remove('active')   
+  }
+
+  blue.onclick = e => {
+    ctx.fillStyle = 'blue'
+    ctx.strokeStyle = 'blue'
+    blue.classList.add('active')
+    red.classList.remove('active')
+    black.classList.remove('active')
+    green.classList.remove('active')  
+  }
+
+  green.onclick = e => {
+    ctx.fillStyle = 'green'
+    ctx.strokeStyle = 'green'
+    green.classList.add('active')
+    red.classList.remove('active')
+    blue.classList.remove('active')
+    black.classList.remove('active')
+  }  
 }
+
